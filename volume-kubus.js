29bdac5130d046adaadb46d95e2474c03b0cc76e
Misbahudin <misbahudin.id@gmail.com>
2023-09-01 22:36:34 +0700

    hitung volume kubus

---
+++ volume-kubus.js
@@ -0,0 +1,11 @@
+function volumeKubus(a,b) {
+    let vA = a ** 3;
+    let vB = b ** 3;
+
+    let hasil = vA + vB
+
+    return hasil
+}
+const hasil = volumeKubus(8,3)
+
+console.log("Volume dari 2 kubus adalah: " + hasil)
